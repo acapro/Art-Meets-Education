a5ff46af6e86a121417012c50d6918e3db2136d9
Adam Cooper <mail@adamcooper.de>
2016-11-23 17:50:48 +0100

    show kids images

--- app/helpers/kids_helper.rb
+++ app/helpers/kids_helper.rb
@@ -2,11 +2,15 @@ module KidsHelper
   def prev_kid(kid)
     if @kid.prev
       link_to 'zurück', page_kid_path(@kid.prev)
+    else
+      'zurück'
     end
   end
   def next_kid(kid)
     if @kid.next
       link_to 'weiter', page_kid_path(@kid.next)
+    else
+      'weiter'
     end
   end
 end
