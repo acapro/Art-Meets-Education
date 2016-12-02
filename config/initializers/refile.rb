2dd66039cbf25372e33e904e37ab4cb60ec43fde
Adam Cooper <mail@adamcooper.de>
2016-12-02 16:01:19 +0100

    Adds cdn info

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -10,3 +10,8 @@ aws = {
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
+
+if Rails.env.production?
+  Refile.cdn_host = Rails.application.secrets.cdn_host
+  Refile.content_max_age = 31536000
+end
