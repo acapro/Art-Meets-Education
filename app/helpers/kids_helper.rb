76a815adedb9163816bbf182a3720f7040953deb
Adam Cooper <mail@adamcooper.de>
2018-02-12 09:12:29 +0100

    Vimeo https

--- app/helpers/kids_helper.rb
+++ app/helpers/kids_helper.rb
@@ -14,10 +14,10 @@ module KidsHelper
       t('nav.next')
     end
   end
-  
+
   def get_vimeo_thumb(video_url)
     vimeo_video_id           = video_url.split('/').last
-    vimeo_video_json_url     = "http://vimeo.com/api/v2/video/%s.json" % vimeo_video_id   # API call
+    vimeo_video_json_url     = "https://vimeo.com/api/v2/video/%s.json" % vimeo_video_id   # API call
     # Parse the JSON and extract the thumbnail_large url
     thumbnail_image_location = JSON.parse(open(vimeo_video_json_url).read).first['thumbnail_large'] rescue nil
     return image_tag thumbnail_image_location
