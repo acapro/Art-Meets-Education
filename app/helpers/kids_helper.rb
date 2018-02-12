1254714a134913354ac24082bfbe24d8cc43affa
Adam Cooper <mail@adamcooper.de>
2018-02-12 09:21:09 +0100

    fixes vimeo urls

--- app/helpers/kids_helper.rb
+++ app/helpers/kids_helper.rb
@@ -20,6 +20,8 @@ module KidsHelper
     vimeo_video_json_url     = "https://vimeo.com/api/v2/video/%s.json" % vimeo_video_id   # API call
     # Parse the JSON and extract the thumbnail_large url
     thumbnail_image_location = JSON.parse(open(vimeo_video_json_url).read).first['thumbnail_large'] rescue nil
+    url = URI.parse(thumbnail_image_location)
+    url.scheme = 'https'
     return image_tag thumbnail_image_location
   end
 end
